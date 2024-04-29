9c6158bfd8fd9b731a1ef5bed60a6576c348dc4c
Kriti Reddy Chinta <125081935+petrichor98765@users.noreply.github.com>
2024-04-29 08:02:41 +0530

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 /**
 * Template Name: Medilab
-* Updated: Jan 29 2024 with Bootstrap v5.3.2
 * Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+* Updated: Mar 17 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
 (function() {
   "use strict";
 
